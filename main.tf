751ff93be4ed56f9bb5fd0c415e46da1b2368463
Robert <robert.sivilli@dds.mil>
2021-12-13 10:19:00 -0700

    bug fix

--- main.tf
+++ main.tf
@@ -160,7 +160,7 @@ data "aws_iam_policy_document" "es_logs" {
   }
   statement {
     actions   = ["logs:PutLogEvents", "logs:PutLogEventsBatch", "logs:CreateLogStream"]
-    resources = ["arn:${aws_partition.current.name}:logs:${aws_region.current.name}:${aws_caller_identity.current.id}:log-group:${format("%s-es-cloudwatch", local.namingprexfix)}", "arn:${aws_partition.current.name}:logs:${aws_region.current.name}:${aws_caller_identity.current.id}:log-group:${format("%s-es-cloudwatch", local.namingprexfix)}*"]
+    resources = ["arn:${data.aws_partition.current.name}:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:log-group:${format("%s-es-cloudwatch", local.namingprexfix)}", "arn:${data.aws_partition.current.name}:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:log-group:${format("%s-es-cloudwatch", local.namingprexfix)}*"]
   }
 }
 
